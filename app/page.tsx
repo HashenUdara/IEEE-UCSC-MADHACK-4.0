f2d9891c89111816d7cd08df7acfaa327cd9e1d2
Hashen Udara <106765093+HashenUdara@users.noreply.github.com>
2024-09-16 23:08:04 +0530

    edit hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,24 @@
 import { CTA } from "@/components/common/cta";
 import FAQ from "@/components/sections/faq";
 import Timeline from "@/components/sections/timeline";
-
+import Image from "next/image";
+import HeroImg from "@/public/img/hero2.jpeg";
 export default function Home() {
   return (
     <div>
+      <div className=" relative min-h-screen w-full">
+        <h1 className=" text-7xl pt-24  px-20 max-w-4xl font-geist-sans font-semibold ">
+          Unleashing Innovation and Creativity in Mobile App Development.{" "}
+        </h1>
+        <Image
+          src={HeroImg}
+          alt=""
+          width={1200}
+          className=" absolute  inset-0  -z-10 object-cover w-full h-full"
+        />
+        {/* <div className=" absolute inset-0 h-full w-full  bg-gradient-to-r from-black"></div> */}
+        <div className=" absolute inset-0 h-full w-full  bg-gradient-to-t from-black"></div>
+      </div>
       Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur ad
       minus accusamus explicabo voluptatem doloribus esse magni quos soluta
       voluptates blanditiis vero quaerat dolores, doloremque voluptatibus cumque

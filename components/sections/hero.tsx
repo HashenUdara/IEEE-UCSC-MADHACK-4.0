7035cde7a90c36265eab9f3053c3402ff54e20bf
Hashen Udara <106765093+HashenUdara@users.noreply.github.com>
2024-09-17 08:03:56 +0530

    Update hero.tsx

--- components/sections/hero.tsx
+++ components/sections/hero.tsx
@@ -4,18 +4,21 @@ import HeroImg from "@/public/img/hero2.jpeg";
 
 const Hero = () => {
   return (
-    <section className=" relative min-h-screen w-full">
-      <h1 className=" text-7xl pt-24  px-20 max-w-4xl font-geist-sans font-semibold ">
-        {/* Unleashing Innovation and Creativity in Mobile App Development.{" "} */}
-      </h1>
+    <section className=" relative flex items-end min-h-screen w-full">
+      <div className="container mx-auto pb-24 pl-10">
+        <h1 className=" text-7xl   max-w-4xl font-geist-sans font-semibold ">
+          Unleashing Innovation and Creativity in Mobile App Development.{" "}
+        </h1>
+      </div>
+
       <Image
         src={HeroImg}
         alt=""
         width={1200}
-        className=" absolute  inset-0  -z-10 object-cover w-full h-full"
+        className=" absolute  inset-0  -z-20 object-cover w-full h-full"
       />
       {/* <div className=" absolute inset-0 h-full w-full  bg-gradient-to-r from-black"></div> */}
-      <div className=" absolute inset-0 h-full w-full  bg-gradient-to-t  from-background"></div>
+      <div className=" absolute inset-0 h-full w-full  -z-10 bg-gradient-to-t  from-background"></div>
     </section>
   );
 };
